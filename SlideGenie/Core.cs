11353d51482c679570fd8f78436b20557741144a
chimingkuei <chimingkuei@gmail.com>
2025-04-30 00:08:07 +0800

    SlideGenie

---
+++ SlideGenie/Core.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Office.Core;
+using Microsoft.Office.Interop.PowerPoint;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlideGenie
+{
+    public class ImagePlacement
+    {
+        public float Left { get; set; }
+        public float Top { get; set; }
+        public float Width { get; set; }
+        public float Height { get; set; }
+
+        public ImagePlacement(float left, float top, float width, float height)
+        {
+            Left = left;
+            Top = top;
+            Width = width;
+            Height = height;
+        }
+    }
+
+    class Core
+    {
+        Application pptApp = new Application();
+
+        private void SlideTitle(Slide slide, string titleText)
+        {
+            float titleLeft = 66;
+            float titleTop = 58;
+            float titleWidth = 828;
+            float titleHeight = 104;
+            var titleBox = slide.Shapes.AddTextbox(
+                Orientation: MsoTextOrientation.msoTextOrientationHorizontal,
+                Left: titleLeft,
+                Top: titleTop,
+                Width: titleWidth,
+                Height: titleHeight
+            );
+            titleBox.TextFrame.AutoSize = (PpAutoSize)MsoTriState.msoFalse;
+            titleBox.TextFrame.TextRange.Text = titleText;
+            titleBox.TextFrame.TextRange.Font.Size = 48;
+            titleBox.TextFrame.TextRange.Font.Bold = MsoTriState.msoTrue;
+            titleBox.TextFrame.TextRange.ParagraphFormat.Alignment = PpParagraphAlignment.ppAlignLeft;
+            titleBox.TextFrame2.VerticalAnchor = MsoVerticalAnchor.msoAnchorMiddle;
+            titleBox.TextFrame.TextRange.Font.Name = "Microsoft JhengHei";
+        }
+
+        private void SlideImage(Slide slide, string imgPath, ImagePlacement placement)
+        {
+            slide.Shapes.AddPicture(
+                FileName: imgPath,
+                LinkToFile: MsoTriState.msoFalse,
+                SaveWithDocument: MsoTriState.msoTrue,
+                Left: placement.Left,
+                Top: placement.Top,
+                Width: placement.Width,
+                Height: placement.Height
+            );
+        }
+
+        /// <summary>
+        /// 1cm = 約28.35pt
+        /// </summary>
+        /// <param name="titles"></param>
+        /// <param name="imagePaths"></param>
+        /// <param name="savePath"></param>
+        public void BuildSlide(string[] titles, string[][] imageGroups, string savePath)
+        {
+            Presentation presentation = pptApp.Presentations.Add(MsoTriState.msoTrue);
+            for (int i = 0; i < titles.Length; i++)
+            {
+                string titleText = titles[i];
+                string[] images = imageGroups[i];
+                // 新增空白頁
+                var slide = presentation.Slides.Add(presentation.Slides.Count + 1, Microsoft.Office.Interop.PowerPoint.PpSlideLayout.ppLayoutBlank);
+                SlideTitle(slide, titleText);
+                // 根據圖片數量配置位置（此處以兩張圖片為例）
+                var placements = new ImagePlacement[]
+                {
+                    new ImagePlacement(100f, 150f, 360f, 270f),  // 左圖
+                    new ImagePlacement(520f, 150f, 360f, 270f)   // 右圖
+                };
+                for (int j = 0; j < images.Length && j < placements.Length; j++)
+                {
+                    SlideImage(slide, images[j], placements[j]);
+                }
+            }
+            presentation.SaveAs(savePath);
+            presentation.Close();
+            pptApp.Quit();
+        }
+
+    }
+}
---
+++ SlideGenie/MainWindow.xaml.cs
@@ -0,0 +1,183 @@
+﻿using Microsoft.Office.Core;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SlideGenie
+{
+    #region Config Class
+    public class SerialNumber
+    {
+        [JsonProperty("Parameter1_val")]
+        public string Parameter1_val { get; set; }
+        [JsonProperty("Parameter2_val")]
+        public string Parameter2_val { get; set; }
+    }
+
+    public class Model
+    {
+        [JsonProperty("SerialNumbers")]
+        public SerialNumber SerialNumbers { get; set; }
+    }
+
+    public class RootObject
+    {
+        [JsonProperty("Models")]
+        public List<Model> Models { get; set; }
+    }
+    #endregion
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region Function
+        private void WindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (MessageBox.Show("請問是否要關閉？", "確認", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        #region Config
+        private SerialNumber SerialNumberClass()
+        {
+            SerialNumber serialnumber_ = new SerialNumber
+            {
+                Parameter1_val = Parameter1.Text,
+                Parameter2_val = Parameter2.Text
+            };
+            return serialnumber_;
+        }
+
+        private void LoadConfig(int model, int serialnumber, bool encryption = false)
+        {
+            List<RootObject> Parameter_info = Config.Load(encryption);
+            if (Parameter_info != null)
+            {
+                Parameter1.Text = Parameter_info[model].Models[serialnumber].SerialNumbers.Parameter1_val;
+                Parameter2.Text = Parameter_info[model].Models[serialnumber].SerialNumbers.Parameter2_val;
+            }
+            else
+            {
+                // 結構:2個Models、Models下在各2個SerialNumbers
+                SerialNumber serialnumber_ = SerialNumberClass();
+                List<Model> models = new List<Model>
+                {
+                    new Model { SerialNumbers = serialnumber_ },
+                    new Model { SerialNumbers = serialnumber_ }
+                };
+                List<RootObject> rootObjects = new List<RootObject>
+                {
+                    new RootObject { Models = models },
+                    new RootObject { Models = models }
+                };
+                Config.SaveInit(rootObjects, encryption);
+            }
+        }
+
+        private void SaveConfig(int model, int serialnumber, bool encryption = false)
+        {
+            Config.Save(model, serialnumber, SerialNumberClass(), encryption);
+        }
+        #endregion
+
+        #region Dispatcher Invoke 
+        public string DispatcherGetValue(System.Windows.Controls.TextBox control)
+        {
+            string content = "";
+            this.Dispatcher.Invoke(() =>
+            {
+                content = control.Text;
+            });
+            return content;
+        }
+
+        public void DispatcherSetValue(string content, System.Windows.Controls.TextBox control)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                control.Text = content;
+            });
+        }
+        #endregion
+        #endregion
+
+        #region Parameter and Init
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadConfig(0, 0);
+        }
+        BaseConfig<RootObject> Config = new BaseConfig<RootObject>();
+        #region Log
+        BaseLogRecord Logger = new BaseLogRecord();
+        //Logger.WriteLog("儲存參數!", LogLevel.General, richTextBoxGeneral);
+        #endregion
+        #endregion
+
+        #region Main Screen
+        private void Main_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            switch ((sender as System.Windows.Controls.Button).Name)
+            {
+                case nameof(Demo):
+                    {
+                        string[] titles = new string[]
+                        {
+                            "瑕疵1",
+                            "瑕疵2",
+                            "瑕疵3"
+                        };
+                        string[][] imageGroups = new string[][]
+                        {
+                            new string[]
+                            {
+                                 @"D:\Chimingkuei\repos\SlideGenie\Testing dataset\Image\Original\1.bmp",
+                                 @"D:\Chimingkuei\repos\SlideGenie\Testing dataset\Image\Processed\1.bmp"
+                            },
+                            new string[]
+                            {
+                                 @"D:\Chimingkuei\repos\SlideGenie\Testing dataset\Image\Original\2.bmp",
+                                 @"D:\Chimingkuei\repos\SlideGenie\Testing dataset\Image\Processed\2.bmp"
+                            },
+                            new string[]
+                            {
+                                @"D:\Chimingkuei\repos\SlideGenie\Testing dataset\Image\Original\3.bmp",
+                                @"D:\Chimingkuei\repos\SlideGenie\Testing dataset\Image\Processed\3.bmp"
+                            }
+                        };
+
+                        string savePath = @"D:\Chimingkuei\repos\SlideGenie\Testing dataset\簡報1.pptx";
+                        Core slide = new Core();
+                        slide.BuildSlide(titles, imageGroups, savePath);
+                        Console.WriteLine("PPT 匯出成功！");
+                        break;
+                    }
+            }
+        }
+        #endregion
+
+
+    }
+}
